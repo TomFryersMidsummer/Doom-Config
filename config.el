b919f7974fbccd68a9a07d16d96aea15db38b514
Tom Fryers <tom.fryers@midsummerenergy.co.uk>
2024-10-23 09:12:24 +0100

    Disable format on save for TypeScript

--- config.el
+++ config.el
@@ -104,7 +104,7 @@
 (setq auth-sources '("~/.authinfo"))
 
 (setq +format-on-save-disabled-modes
-      '(emacs-lisp-mode sql-mode tex-mode latex-mode org-msg-edit-mode rjsx-mode))
+      '(emacs-lisp-mode sql-mode tex-mode latex-mode org-msg-edit-mode rjsx-mode typescript-mode))
 
 (use-package dap-mode
   :defer t
