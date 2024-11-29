91a9dcef76a09223d418d8ad80f03b3a959add28
Tom Fryers <tom.fryers@midsummerenergy.co.uk>
2024-11-29 11:47:42 +0000

    Add pr-review package

--- config.el
+++ config.el
@@ -195,3 +195,9 @@
 
 (setq ispell-dictionary "en_GB")
 (setq frame-title-format "%b – Emacs")
+
+(evil-ex-define-cmd "prr" #'pr-review)
+(evil-ex-define-cmd "prs" #'pr-review-search)
+(evil-ex-define-cmd "prn" #'pr-review-notification)
+(add-to-list 'browse-url-default-handlers
+             '(pr-review-url-parse . pr-review-open-url))
