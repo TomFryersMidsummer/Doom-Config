128c4efc542f7726bce02f0e3a5979f8b3f0f6cb
Tom Fryers <tom.fryers@midsummerenergy.co.uk>
2025-09-15 12:05:29 +0100

    Enable tramp module

--- init.el
+++ init.el
@@ -71,6 +71,7 @@
        dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ;;ibuffer         ; interactive buffer management
+       tramp             ; remote files at your arthritic fingertips
        undo              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
